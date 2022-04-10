eab811753b24ed8aae5a81ed03d9191a11525628
Juan Jose Alejo <juanjose.alejofalcon@nttdata.com>
2022-04-10 13:54:04 -0500

    Microservicio para gestion de Productos pasivos

---
+++ src/main/java/com/bootcamp/passiveproducts/PassiveproductsApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.passiveproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassiveproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassiveproductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/passiveproducts/PassiveproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.passiveproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassiveproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
